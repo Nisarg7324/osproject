fdc9962b60a44acc86a672e21cdebe75627137a1
neevshah1273 <58587061+neevshah1273@users.noreply.github.com>
2020-09-21 21:59:12 +0530

    Add files via upload

--- SJF.dart
+++ SJF.dart
@@ -1,21 +1,108 @@
 import 'package:flutter/material.dart';
 
+import './main.dart';
 
-//SJF page stateful class
-class SJF extends StatefulWidget {
-  @override
-  _SJFState createState() => _SJFState();
-}
+class SJF extends StatefulWidget{
 
-class _SJFState extends State<SJF> {
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Text('SJF'),
-          backgroundColor: Colors.red,
-        ),
-      );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return sjfstate();
   }
 }
+
+class sjfstate extends State{
+  var _counter=1;
+  List<DataRow> _rowList = [
+    DataRow(cells: <DataCell>[
+      DataCell(Text('P1')),
+      DataCell(TextField(
+        maxLines: 1,
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+      )),
+      DataCell(TextField(
+        maxLines: 1,
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+      )),
+      DataCell(Text('0')),
+      DataCell(Text('0')),
+      DataCell(Text('0')),
+    ]),
+  ];
+  void _addrow(){
+    setState(() {
+      _counter++;
+      _rowList.add(DataRow(
+          cells: <DataCell>[
+            DataCell(Text('P'+_counter.toString())),
+            DataCell(TextField(
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+            )),
+            DataCell(TextField(
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+            )),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+            DataCell(Text('0')),
+          ]
+      ));
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('SJF'),
+      ),
+      body: Container(
+          child: Column(
+              children: <Widget>[
+                Container(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: DataTable(
+                          columns: [
+                            DataColumn(label: Text('P'),numeric: false),
+                            DataColumn(label: Text('AT'),numeric: true),
+                            DataColumn(label: Text('BT'),numeric: true),
+                            DataColumn(label: Text('CT'),numeric: true),
+                            DataColumn(label: Text('TAT'),numeric: true),
+                            DataColumn(label: Text('WT'),numeric: true),
+                          ],
+                          rows: _rowList,
+                        ),
+                      ),
+                    )
+                ),
+                Row(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: (
+                          FloatingActionButton(
+                            backgroundColor: Colors.red,
+                            child: Icon(
+                              Icons.add,
+                            ),
+                            onPressed: _addrow,
+                          )
+                      ),
+                    )
+                  ],
+                ),
+
+              ])
+      ),
+    );
+  }
+}
+
